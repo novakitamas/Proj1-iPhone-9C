24b4bda36b3f4e1b7423d10f7de6704f80e7a5cf
novakitamas <novaki.tamas@students.jedlik.eu>
2023-03-06 11:46:25 +0100

    kész,+ zip kód javítása

--- kontakt.js
+++ kontakt.js
@@ -11,9 +11,17 @@
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
-      } else  
+      } else
         alert("Köszönjük a visszajelzést, hamarosan felvesszük önnel a kapcsolatot!")
       form.classList.add('was-validated')
     }, false)
   })
 })()
+document.querySelector("#zip").addEventListener("keypress", function (evt) {
+  if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+  {
+      evt.preventDefault();
+  }
+});
+
+  
